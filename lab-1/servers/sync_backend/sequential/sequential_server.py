dc4ca37529d66773c31e982b65158ee0a5639738
Anas Awadallah <anas.bekheit@gmail.com>
2025-02-12 23:33:55 +0200

    Remove socket backlogs

--- lab-1/servers/sync_backend/sequential/sequential_server.py
+++ lab-1/servers/sync_backend/sequential/sequential_server.py
@@ -58,7 +58,7 @@ def run_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Allow reuse of address
     server_socket.bind((host, port))
-    server_socket.listen(1)
+    server_socket.listen()
     logger.info(f"Server started on {host}:{port}")
 
     try:
